11bd821cc17b0add22d9a5bc771cd1497faa0b1b
jacktan1016 <402192080@qq.com>
2020-03-05 23:24:53 +0800

    reqiester 跑通

---
+++ meiduo_mall/meiduo_mall/apps/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+"""
+@author: JakeTan
+@file: __init__.py.py
+@time: 2020/3/5 18:39
+@desc: 
+"""
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
+# sys.path 优先找当前目录,存的是导包路径.它是一个列表
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -29,6 +31,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+# sys.path.append(os.path.join(BASE_DIR, ...))
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 完整路径
+    # "meiduo_mall.apps.users.UsersConfig",
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -195,3 +202,5 @@ LOGGING = {
         },
     }
 }
+# 指定用户模型类
+# AUTH_USER_MODEL = 'users.User'  # 应用名称.模型类名称
